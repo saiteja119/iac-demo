5237134f31493c4b80aa78c89faff0d5b59b8772
saiteja119 <60913989+saiteja119@users.noreply.github.com>
2022-05-02 11:38:11 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -41,7 +41,6 @@ resource "aws_route_table" "route_table" {
 resource "aws_subnet" "subnet" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = ap-south-1a
 
   tags = {
     Name = "prod-subnet"
